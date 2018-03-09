ad9ee54b7bf33a8d87468a5e0e6f053b0ae15931
Brendan <brendan.lesniak@gmail.com>
2018-03-09 12:42:55 -0600

    Added more images, help
    Added the final set of maps to the local cache. Added
    the help text on how to use the app. Next steps will be
    to finish the Marker Management, and add the calculation
    functions. Future work will be to incorperate height maps
    to make the calcuator more accurate.

--- SquadAndroidCalculator/app/src/main/java/com/squadfinder/brend/squadandroidcalculator/activity/MainActivity.java
+++ SquadAndroidCalculator/app/src/main/java/com/squadfinder/brend/squadandroidcalculator/activity/MainActivity.java
@@ -1,7 +1,10 @@
 package com.squadfinder.brend.squadandroidcalculator.activity;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -22,49 +25,53 @@ import com.squadfinder.brend.squadandroidcalculator.util.RawResourceLoader;
 import org.json.JSONObject;
 
 public class MainActivity extends BaseActivity {
-    // Progress Dialog for user feedback of requests
-    private ProgressDialog pDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        // Inflate the view so we can add dynamic elements
-        View mainView = getLayoutInflater().inflate(R.layout.activity_main, null);
-
-        // Load Map Data and set our View
-        loadMapData(mainView);
-        setContentView(mainView);
-
-        // Setup our Help Button
-        ImageView helpImage = mainView.findViewById(R.id.helpButton);
-        helpImage.setOnTouchListener((View v, MotionEvent event) -> true);
-
-        // Toast!
-        Toast.makeText(this, "Welcome to Squad Finder's Mortar Calculator!", Toast.LENGTH_LONG).show();
-    }
-
-    @SuppressLint("ResourceType")
-    private void addScrollViewContent(View view, SquadMap[] maps) {
-        ArrayAdapter<SquadMap> mapAdapter = new ListViewMapAdapter(this, maps);
-        ListView mapList = view.findViewById(R.id.mapListView);
-        mapList.setAdapter(mapAdapter);
-        mapList.setOnItemClickListener(new ListViewMapListener(this));
-    }
-
-    private void loadMapData(View view) {
-        pDialog = new ProgressDialog(this);
-        pDialog.setMessage("Loading...");
+        // Load Map Data
+        ProgressDialog pDialog = new ProgressDialog(this);
+        pDialog.setMessage(getString(R.string.str_loading));
         pDialog.show();
         simpleGetWebJSON("", (JSONObject json) -> {
             Log.d("JSON_REQUEST", String.format("Got JSON: %s", json.toString()));
-            addScrollViewContent(view, new Gson().fromJson(json.toString(), SquadMap[].class));
+            addScrollViewContent(this, new Gson().fromJson(json.toString(), SquadMap[].class));
             pDialog.dismiss();
         }, (VolleyError error) -> {
             Log.d("JSON_REQUEST", String.format("Error: %s", error.toString()));
             String json = RawResourceLoader.readRawResourceAsString(getResources().openRawResource(R.raw.maps_default));
-            addScrollViewContent(view, new Gson().fromJson(json, SquadMap[].class));
+            addScrollViewContent(this, new Gson().fromJson(json, SquadMap[].class));
             pDialog.dismiss();
         });
+
+        // Setup our Help Button
+        ImageView helpImage = findViewById(R.id.helpButton);
+        helpImage.setOnTouchListener((View v, MotionEvent event) -> {
+            v.performClick();
+            AlertDialog dialog = buildAlertDialog();
+            dialog.show();
+            return false;
+        });
+
+        // Welcome to the App
+        Toast.makeText(this, R.string.str_welcome, Toast.LENGTH_LONG).show();
+    }
+
+    private AlertDialog buildAlertDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        return builder.setTitle(R.string.str_help_title)
+                .setMessage(R.string.str_help)
+                .setCancelable(true)
+                .setNeutralButton(R.string.str_ok, null)
+                .create();
+    }
+
+    private void addScrollViewContent(Activity context, SquadMap[] maps) {
+        ArrayAdapter<SquadMap> mapAdapter = new ListViewMapAdapter(this, maps);
+        ListView mapList = context.findViewById(R.id.mapListView);
+        mapList.setAdapter(mapAdapter);
+        mapList.setOnItemClickListener(new ListViewMapListener(this));
     }
 }
